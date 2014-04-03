1375eba39fde482f32a0ca710d5881b292714155
anhlt <tuananh.kirimaru@gmail.com>
2014-04-03 17:15:39 +0700

    Config RQ

--- GR3/urls.py
+++ GR3/urls.py
@@ -2,5 +2,8 @@ from django.conf.urls import patterns, url
 from GR3 import views
 
 urlpatterns = patterns('',
-    url(r'^$', views.current_datetime),
+    url(r'^job/', views.get_result),
+    url(r'', views.current_datetime),
+    
+
 )
--- GR3/views.py
+++ GR3/views.py
@@ -1,18 +1,18 @@
-from django.shortcuts import render
-from django.http import HttpResponse
+import json
+import logging
 
-import datetime
+from django.http import HttpResponse
+import requests
+import django_rq
+from django_rq import job
+from rq.job import Job
+
 from recommendation.model.classes import MatrixPreferenceDataModel as DataModel
 from recommendation.metrics.pairwise import euclidean_distances
 from recommendation.similarities.basic_similarities import ItemSimilarity
 from recommendation.recommender.classes import ItemBasedRecommender
 from recommendation.recommender.item_strategies import ItemsNeighborhoodStrategy
 
-import json
-import requests
-import logging
-import django_rq
-from django_rq import job
 
 logging.basicConfig(level=logging.DEBUG)
 logger = logging.getLogger('Viewer')
@@ -21,12 +21,29 @@ logger = logging.getLogger('Viewer')
 base_api_url = "http://harrenhal-php-97705.apse1.nitrousbox.com/elgg/services/api/rest/json/"
 
 def current_datetime(request):
-    job = django_rq.enqueue(recommended,287)
+    user_id = int(request.GET.get('user_id','287'))
+    job = django_rq.enqueue(recommended,user_id)
+    logger.debug(job.key)
+    response_data = {'status':'processing', 'job': job.key}
+    return HttpResponse(json.dumps(response_data), content_type="application/json")
+    # return HttpResponse(job.key)
 
-    logger.debug(job.result())
+def get_result(request):
+    job_id = request.GET.get('job_id')
+    redis_conn = django_rq.get_connection()
+    job_id=job_id.split(':')[2]
+    logger.info(job_id)
+    job = Job.fetch(job_id,redis_conn)
+    if job.is_finished:
+        ret = job.return_value
+    elif job.is_queued:
+        ret = {'status':'in-queue'}
+    elif job.is_started:
+        ret = {'status':'waiting'}
+    elif job.is_failed:
+        ret = {'status': 'failed'}
+    return HttpResponse(json.dumps(ret), content_type="application/json")
 
-    # return HttpResponse(json.dumps(response_data), content_type="application/json")
-    return HttpResponse("Hello ")
 
 @job
 def recommended(user_id):
@@ -35,7 +52,7 @@ def recommended(user_id):
     results = r.json()
     results = results['result']
     dataset = {}
-    for result in results[:2000]:
+    for result in results:
        if result['rating'] != 0:
             user_id = result['user_id']
             if user_id not in dataset:
@@ -46,5 +63,4 @@ def recommended(user_id):
     items_strategy = ItemsNeighborhoodStrategy()
     recsys = ItemBasedRecommender(model, similarity, items_strategy)
     response_data = recsys.recommended(user_id)
-    logger.debug(response_data)
     return response_data
